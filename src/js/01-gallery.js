ebff66902c78d8d1f6cf2176abe7934e125ede72
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-02-19 12:54:38 +0200

    Add task #1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,40 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemEl = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemEl);
+
+function createGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => 
+        `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    ).join("");
+}
+
+let lightBox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250
+});
+
+lightBox.on('show.simplelightbox', function () {
+    console.log(lightBox);
+});
+
+lightBox.on('error.simplelightbox', function (event) {
+	console.log(event); // some usefull information
+});
+
+/*
+
+1. Add the SimpleLightbox library as a project dependency using npm.
+2. Use your JavaScript code from the previous homework, 
+but refactor it given that the library was installed via npm (import/export syntax).
+
+*/
