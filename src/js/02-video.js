b78b3829e2802775bba51e2c3495fa5da5d93c4f
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-02-19 20:45:57 +0200

    Adds task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,32 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+function currentTime(data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+}
+
+let selectedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+player.setCurrentTime(selectedTime).then(function(seconds) {
+  // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+  switch (error.name) {
+    case 'RangeError':
+      // the time was less than 0 or greater than the video’s duration
+      break;
+
+    default:
+      // some other error occurred
+      break;
+  }
+});
+
 /*
 1. Check out the documentation of the Vimeo player library.
 2. Add the library as a project dependency via npm.
