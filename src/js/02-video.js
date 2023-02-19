f2564b6277c6c071f58553490863b2347a93e8f9
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-02-19 13:00:19 +0200

    Updated files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
-
+/*
+1. Check out the documentation of the Vimeo player library.
+2. Add the library as a project dependency via npm.
+3. Initialize the player in the script file as described in the pre-existing player section, 
+but note that you have added the player as an npm package, not via CDN.
+4. Read the documentation of the on() method and start tracking the timeupdate event - playback time update.
+5. Save playback time to local storage. 
+Let the key for the storage be the "videoplayer-current-time" string.
+6. When reloading the page, use the setCurrentTime() method 
+to resume playback from the saved position.
+7. Add the lodash.throttle library to the project and make sure that the playback time is updated
+in the storage once a second or less frequent.
+*/
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
-
+/*
+1. Track the input event on the form, and each time write to local storage an object with the email and message fields, 
+in which you save the current values of the form fields. 
+Let the key for the storage be the "feedback-form-state" string.
+2. When loading the page, check the state of the storage, and if it stores some data, 
+use it to fill in the form fields. Otherwise, the fields must be empty.
+3. When submitting the form, clear the storage and form fields, 
+and also display the object with the email and message fields and their current values in the console.
+4. Make sure that the storage is updated no more than once every 500 milliseconds. 
+To do this, add to the project and use the lodash.throttle library.
+*/
